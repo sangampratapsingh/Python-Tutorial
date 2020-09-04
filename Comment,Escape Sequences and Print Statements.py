4a53d41cc94abe0c106af10a07401ad2d47693f5
sangampratapsingh <70759399+sangampratapsingh@users.noreply.github.com>
2020-09-04 18:24:09 +0530

    Create Comment,Escape Sequences and Print Statements.py

---
+++ Comment,Escape Sequences and Print Statements.py
@@ -0,0 +1,41 @@
+#print("Hello World")
+
+#print("Hello Sangam")
+
+#Commonly Used escape sequences
+
+# \\:insert backslash(\) in the text at that point.
+
+# \":Insert double quote(") character in the text at that point.
+
+# \':Insert single quote(') character in the text at that point.
+
+# \t:Insert a tab in the text at that point.
+
+# \f:Insert a form feed in the text at that point.
+
+# \r:Insert carriage return in the text at that point.
+
+# \b:Insert backspace in the text at that point.
+
+# end(""):This refers if we print two lines we can join these lines, and adjust them with space,comma,fullstop etc.
+
+print("1.Sangam \n Happy Birthday")
+
+print("2.Sangam \\ Happy Birthday")
+
+print("3.Sangam \" Happy Birthday")
+
+print("4.Sangam \' Happy Birthday")
+
+print("5.Sangam \t Happy Birthday")
+
+print("6.Sangam \f Happy Birthday")
+
+print("7.Sangam \r Happy Birthday")
+
+print("8.Sangam \b Happy Birthday")
+
+print("9.Sangam", end=(""))
+
+print("Happy Birthday")
