de8cb4845df21013a9aebbeb3f798e02df5d4d92
Sangam Pratap Singh <70759399+sangampratapsingh@users.noreply.github.com>
2020-09-04 18:42:40 +0530

    Create Break and Continue Statements in Python.py

---
+++ Break and Continue Statements in Python.py
@@ -0,0 +1,19 @@
+"""i = 0
+while(True):
+i = i + 1
+if i+1<5:
+continue
+print(i+1, end=" ")
+if (i==44):
+i = i + 1
+break#stop the loop"""
+#Quiz
+
+
+while(True):
+inp = int(input("enter your number :"))
+if inp>100:
+print("congratulations")
+break
+else:
+continue
