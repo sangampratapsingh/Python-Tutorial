e6a43f76faef8881bbcf83fb21680916368ba441
Sangam Pratap Singh <70759399+sangampratapsingh@users.noreply.github.com>
2020-09-04 18:37:40 +0530

    Create Variables,Datatypes and Typecasting.py

---
+++ Variables,Datatypes and Typecasting.py
@@ -0,0 +1,20 @@
+var1 = "Hello World"
+var2 = 4
+var3 = 36.7
+print(var1)
+print(var2)
+print(var3)
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(var2+var3)
+
+#Practice Question
+
+print("What is your first number")
+n1 = input()
+
+print("What is your second number")
+n2 = input()
+
+print("The sum of these numbers is", int(n1)+int(n2))
