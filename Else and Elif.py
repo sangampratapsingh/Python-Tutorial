14acafb483aed60d19d77086abe82eb000583bc3
Sangam Pratap Singh <70759399+sangampratapsingh@users.noreply.github.com>
2020-09-04 18:40:06 +0530

    Create Else and Elif.py

---
+++ Else and Elif.py
@@ -0,0 +1,28 @@
+Else and Elif.py
+#var1 = 56
+#var2 = 64
+
+#print("type your number")
+#var3 = int(input())
+#if var3>var2:
+#print("it is greater")
+#elif var3==var2:
+# print("it is equal")
+
+#else:
+# print("it is smaller")
+print("What is your age")
+age1 = int(18)
+age2 = int(100)
+age3 = int(6)
+
+age4 = int(input())
+
+if age4==age1:
+print("we will think")
+
+elif age2>age4>age1:
+print("can drive")
+
+else:
+print("you cant drive")
